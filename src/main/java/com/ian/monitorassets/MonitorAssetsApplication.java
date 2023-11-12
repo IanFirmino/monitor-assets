ec7962e08f80875f07dfa1fe46ed271afb36e4ad
ian firmino <ian.firmino@hotmail.com>
2023-11-12 18:54:48 -0300

    init

---
+++ src/main/java/com/ian/monitorassets/MonitorAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.ian.monitorassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorAssetsApplication.class, args);
+	}
+
+}
